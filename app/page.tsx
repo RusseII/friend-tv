43fd64d708c34fcfdbb73556ecb4ff61e0a5c019
bookland <rratcliffe57@gmail.com>
2024-02-23 08:17:34 +0800

    update meta

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import { Metadata } from "next";
 import Videos from "./Videos";
 
 export default function Home() {
   return <Videos />;
 }
+
+export const metadata: Metadata = {
+  title: "Friend Videos",
+  description: "This is a compliation of videos sent to me by my friends.",
+};
